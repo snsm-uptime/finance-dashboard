7fab5dcc7ebd039703d3e7a6fa14b1abe9665a5f
Sebastian Soto M <s.m.sebastian.n@gmail.com>
2026-08-14 19:05:37 -0600

    wrm: function

--- lnx-tools.sh
+++ lnx-tools.sh
@@ -1,3 +1,15 @@
+wrm() {
+  local primary_root
+  primary_root="$(git rev-parse --show-toplevel 2>/dev/null)" ||
+    {
+      echo "wrm: not inside a git repo" >&2
+      return 1
+    }
+  "$primary_root/scripts/worktree/worktree-remove.sh" "$@" || return $?
+  export ROOT_WORKTREE_PATH="$primary_root"
+  echo "==> ROOT_WORKTREE_PATH=$ROOT_WORKTREE_PATH"
+}
+
 wad() {
   local primary_root
   primary_root="$(git rev-parse --show-toplevel 2>/dev/null)" ||
